3c6630bb09a700cf5a5242b276b27da5480ba482
Sukhbir Singh Sardar <76410023+Sukhbir0731@users.noreply.github.com>
2025-12-11 16:23:42 -0500

    Time: 0 ms (100.00%) | Memory: 41.9 MB (45.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,19 +1,16 @@
-// Recursive approach
 class Solution {
-    int dp[];
-    public int f(int n){
-        if (n <= 2){
-            return n;
-        }
-        if (dp[n] != -1){
-            return dp[n];
-        }
-        dp[n] = f(n-1) + f(n-2);
-        return dp[n];
-    }
     public int climbStairs(int n) {
-        dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return f(n);
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 }
