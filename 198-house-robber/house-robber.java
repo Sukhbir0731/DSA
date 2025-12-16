3c88e8724c9201eb8b2d1d9d92e4d9884eb61659
Sukhbir Singh Sardar <76410023+Sukhbir0731@users.noreply.github.com>
2025-12-16 14:52:32 -0500

    Time: 0 ms (100.00%) | Memory: 43 MB (7.95%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,16 @@
+// iterative approach
 class Solution {
-    int[] nums;
     int[] dp;
     public int rob(int[] nums) {
         if (nums.length == 1) return nums[0];
-        this.nums = nums;
         dp = new int[nums.length];
         Arrays.fill(dp, -1);
-
         dp[0] = nums[0];
         dp[1] = Math.max(nums[1], nums[0]);
-        return f(2);
-    }
-    public int f(int i){
-        if (i >= nums.length) return dp[nums.length - 1];
-        if (dp[i] == -1) dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
-        return f(i+1);
+        if (nums.length == 2) return dp[1];
+        for(int i=2; i<nums.length; i++){
+            dp[i] = Math.max(nums[i]+dp[i-2], dp[i-1]);
+        }
+        return dp[nums.length-1];
     }
 }
