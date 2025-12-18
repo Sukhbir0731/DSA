86e9ff8fea0a47c558d2152bd8255cdc01d6601c
Sukhbir Singh Sardar <76410023+Sukhbir0731@users.noreply.github.com>
2025-12-17 19:15:19 -0500

    Time: 0 ms (100.00%) | Memory: 43 MB (11.03%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,24 +1,24 @@
 import java.util.Arrays;
 
 class Solution {
-    // iterative approach
+    // iterative approach - space optimized
     // no global initializations
     public int rob(int[] nums) {
         int n = nums.length;
         if (n == 1) return nums[0];
         if (n == 2) return Math.max(nums[0], nums[1]);
         int[] arr1 = Arrays.copyOfRange(nums, 0, n - 1);
-        int[] dp1 = new int[arr1.length];
         int[] arr2 = Arrays.copyOfRange(nums, 1, n);
-        int[] dp2 = new int[arr2.length];
-        return Math.max(f(arr1, dp1), f(arr2, dp2));
+        return Math.max(f(arr1), f(arr2));
     }
-    private int f(int[] arr, int[] dp) {
-        dp[0] = arr[0];
-        dp[1] = Math.max(arr[0], arr[1]);
+    private int f(int[] arr) {
+        int a = arr[0];
+        int b = Math.max(arr[0], arr[1]);
         for(int i=2; i<arr.length; i++){
-            dp[i] = Math.max(dp[i-2]+arr[i], dp[i-1]);
+            int temp = b;
+            b = Math.max(a+arr[i], b);
+            a = temp; 
         }
-        return dp[arr.length-1];
+        return b;
     }
 }
