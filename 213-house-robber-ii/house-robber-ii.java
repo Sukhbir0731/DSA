4d18e6c330af890a82b50a21dc25c33103312014
Sukhbir Singh Sardar <76410023+Sukhbir0731@users.noreply.github.com>
2025-12-17 19:12:25 -0500

    Time: 0 ms (100.00%) | Memory: 43.1 MB (11.03%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,26 +1,24 @@
 import java.util.Arrays;
 
 class Solution {
+    // iterative approach
     // no global initializations
     public int rob(int[] nums) {
         int n = nums.length;
         if (n == 1) return nums[0];
         if (n == 2) return Math.max(nums[0], nums[1]);
         int[] arr1 = Arrays.copyOfRange(nums, 0, n - 1);
+        int[] dp1 = new int[arr1.length];
         int[] arr2 = Arrays.copyOfRange(nums, 1, n);
-        return Math.max(memoizedSolve(arr1), memoizedSolve(arr2));
+        int[] dp2 = new int[arr2.length];
+        return Math.max(f(arr1, dp1), f(arr2, dp2));
     }
-    private int memoizedSolve(int[] arr) {
-        int[] dp = new int[arr.length];
-        Arrays.fill(dp, -1); // Fill with -1 to distinguish from a 0 result
-        return f(arr.length - 1, arr, dp);
-    }
-    private int f(int i, int[] arr, int[] dp) {
-        if (i < 0) return 0;
-        if (i == 0) return arr[0];
-        if (dp[i] != -1) return dp[i];
-        int rob = arr[i] + f(i - 2, arr, dp);
-        int skip = f(i - 1, arr, dp);
-        return dp[i] = Math.max(rob, skip);
+    private int f(int[] arr, int[] dp) {
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+        for(int i=2; i<arr.length; i++){
+            dp[i] = Math.max(dp[i-2]+arr[i], dp[i-1]);
+        }
+        return dp[arr.length-1];
     }
 }
